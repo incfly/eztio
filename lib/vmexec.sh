ce19c5df4dfe13979e2259a4f9c4e06a3b2b7c69
Jianfei Hu <jianfeih@google.com>
2019-05-15 14:29:05 -0700

    dnsmasq not working.

--- lib/vmexec.sh
+++ lib/vmexec.sh
@@ -2,16 +2,17 @@
 
 function meshexp_dnsinit() {
   # dns init
-  export $(cat mesh-expansion.env | xargs)
-  sudo apt-get --no-install-recommends -y install dnsmasq
+  export $(cat meshexp.env | xargs)
+  # skip dns stuff first...
+  # sudo apt-get update
+  # sudo apt-get --no-install-recommends -y install dnsmasq
   # Copy config files for DNS
-  chmod go+r "kubedns"
-  sudo cp "kubedns" /etc/dnsmasq.d
-  sudo systemctl restart dnsmasq
-
+  # chmod go+r "kubedns"
+  # sudo cp "kubedns" /etc/dnsmasq.d
+  # sudo systemctl restart dnsmasq
+  echo "$GATEWAY_IP istio-citadel istio-pilot istio-pilot.istio-system" | sudo tee -a /etc/hosts
   curl "https://storage.googleapis.com/istio-release/releases/1.1.0/deb/istio-sidecar.deb"  -L > istio-sidecar.deb
   sudo dpkg -i istio-sidecar.deb
-  # echo "$GATEWAY_IP istio-citadel istio-pilot istio-pilot.istio-system" | sudo tee -a /etc/hosts
   sudo mkdir -p /etc/certs /var/lib/istio/envoy
   sudo cp {root-cert.pem,cert-chain.pem,key.pem} /etc/certs
   sudo cp cluster.env /var/lib/istio/envoy
